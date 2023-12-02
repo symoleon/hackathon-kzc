662ebb4122f658052535a7cedb42615f950c8b83
draszec <draszec@gmail.com>
2023-12-02 14:35:29 +0100

    add analyze_comment

--- back/Comments.py
+++ back/Comments.py
@@ -4,14 +4,27 @@ from pydantic import BaseModel
 
 from Sentiment import Sentiment
 
+import chat_service
+
 
 class Comment(BaseModel):
     user_id: str
     content: str
-    product: str
 
 
 class AnalyzedComment(Comment):
     date: datetime
     sentiment: Sentiment
-    tags: []
+    tag: list[str]
+
+
+def analyze_comment(comment: Comment) -> AnalyzedComment:
+
+    analyzed_comment = AnalyzedComment.model_validate({
+        **comment.model_dump(),
+        "date": datetime.today(),
+        "sentiment": chat_service.chat_request_sentiment(comment.content),
+        "tags": chat_service.chat_request_tag(comment.content)
+    })
+
+    return analyzed_comment
