bcbca054808b69279949cf9e76418e260a172066
symoleon <symoleon97@gmail.com>
2023-12-02 14:04:16 +0100

    Add more output

--- front/app/api/comments/route.ts
+++ front/app/api/comments/route.ts
@@ -1,11 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 
 const comments = [
-  { "id": 1, "username": "Smakosz123", "opinion": "Bardzo smaczne jedzenie, obsługa miła i profesjonalna. Polecam!", "sentiment": "positive", "date": 1642245600, "tags": ["FOOD", "SERVISE"] },
-  { "id": 2, "username": "GourmetGirl", "opinion": "Restauracja ma przyjemny klimat, dania są świeże i smaczne. Wrócę tam na pewno.", "sentiment": "positive", "date": 1645394400, "tags": ["ATMOSPHERE", "FOOD"] },
-  { "id": 3, "username": "RozczarowanyKlient", "opinion": "Niestety, doświadczenie było rozczarowujące. Dania były zimne, a obsługa nieprzyjazna.", "sentiment": "negative", "date": 1646853600, "tags": ["FOOD", "SERVICE"] },
-  { "id": 4, "username": "DataNightExpert", "opinion": "Doskonałe miejsce na randkę! Menu jest zróżnicowane, a dania są estetycznie podane.", "sentiment": "positive", "date": 1649172000, "tags": ["ATMOSPHERE"] },
-  { "id": 5, "username": "KrytycznyKonsument", "opinion": "Obsługa była bardzo uprzejma, ale jedzenie nie spełniło moich oczekiwań. Może to był pechowy dzień.", "sentiment": "negative", "date": 1652361600, "tags": ["FOOD", "SERVICE"] }
+  { "id": 1, "username": "CulinaryExplorer", "opinion": "Fantastyczne jedzenie, obsługa na najwyższym poziomie!", "sentiment": "positive", "date": 1672531200, "tags": ["FOOD", "SERVICE"] },
+  { "id": 2, "username": "EpicureanAdventurer", "opinion": "Wspaniała atmosfera, dania są prawdziwym dziełem sztuki kulinarniej.", "sentiment": "positive", "date": 1672617600, "tags": ["ATMOSPHERE", "FOOD"] },
+  { "id": 3, "username": "CriticalCuisineCritic", "opinion": "Niestety, jedzenie było zbyt drogie jak na jakość. Obsługa była jednak miła.", "sentiment": "negative", "date": 1672704000, "tags": ["FOOD", "COST", "SERVICE"] },
+  { "id": 4, "username": "GourmetGlobetrotter", "opinion": "Menu było bogate i zróżnicowane. Polecam dla miłośników eksperymentów kulinarnych.", "sentiment": "positive", "date": 1672790400, "tags": ["FOOD", "MENU"] },
+  { "id": 5, "username": "PickyPalate", "opinion": "Jedzenie było zimne, atmosfera dość przeciętna. Nie polecam.", "sentiment": "negative", "date": 1672876800, "tags": ["FOOD", "ATMOSPHERE", "SERVICE"] },
+  { "id": 6, "username": "FoodieNomad", "opinion": "Obsługa była bardzo uprzejma, a dania smakowały wyśmienicie. Wrócę tam na pewno!", "sentiment": "positive", "date": 1672963200, "tags": ["FOOD", "SERVICE"] },
+  { "id": 7, "username": "GastronomyGuru", "opinion": "Ceny były wysokie, ale atmosfera i obsługa zrekompensowały to w pewnym stopniu.", "sentiment": "neutral", "date": 1673049600, "tags": ["COST", "ATMOSPHERE", "SERVICE"] },
+  { "id": 8, "username": "SensorySommelier", "opinion": "Menu było ograniczone, ale jedzenie było doskonałe. Obsługa również na medal.", "sentiment": "positive", "date": 1673136000, "tags": ["FOOD", "MENU", "SERVICE"] },
+  { "id": 9, "username": "EpicureanExplorer", "opinion": "Przeciętne jedzenie, ale miła atmosfera sprawiła, że wieczór był udany.", "sentiment": "positive", "date": 1673222400, "tags": ["FOOD", "ATMOSPHERE"] },
+  { "id": 10, "username": "SelectiveSavorer", "opinion": "Najlepsze dania, jakie kiedykolwiek jadłem! Obsługa również godna polecenia.", "sentiment": "positive", "date": 1673308800, "tags": ["FOOD", "SERVICE"] }
 ];
 
 
