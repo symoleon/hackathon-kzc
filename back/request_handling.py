cc9abd5ca48e474ad050c8e1c209c924c14349ef
draszec <draszec@gmail.com>
2023-12-02 14:34:36 +0100

    add posting a comment add getting all comments (from user's perspective)

--- back/request_handling.py
+++ back/request_handling.py
@@ -1,24 +1,35 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from datetime import datetime
-from Sentiment import Sentiment
+from Comments import Comment, AnalyzedComment, analyze_comment
+import json
+import sqlite3
 
-# import chat_service
+con = sqlite3.connect("comments.db")
+cur = con.cursor()
+# cur.execute("CREATE TABLE comment(username, content, post_date, sentiment, tags)")
 
 app = FastAPI()
 
-class Comment(BaseModel):
-    user_id: str
-    content: str
 
-class Analized_comment:
-    user_id: str
-    content: str
-    date: datetime
-    sentiment: Sentiment
-    tags: []
+# def send_comment_to_db(comment : AnalyzedComment):
+#     with open("comments.json", "a") as file:
+#         file.write(comment.model_dump_json())
 
-    def __init__(self):
-        self.date = datetime.today()
+def send_comment_to_db(comment: AnalyzedComment):
+    cur.execute(
+        f"insert into comment values({comment.user_id}, {comment.content}, {comment.date}, {comment.sentiment}, {comment.tag})")
+    con.commit()
 
 
+@app.get("/customer/comment")
+def see_comments():
+    res = cur.execute("select username, content, post_date from comment")
+    return json.dumps(res.fetchall())
+
+@app.post("/customer/comment")
+def comment(comment: Comment):
+    analyzed_comment = analyze_comment(comment)
+    send_comment_to_db(analyzed_comment)
+
+
+# @app.get("/admin/analyze")
+# def analyze():
