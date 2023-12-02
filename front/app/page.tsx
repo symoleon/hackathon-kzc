4b7587ea6d2bff5ba4c1be323620332a8bebfcd4
symoleon <symoleon97@gmail.com>
2023-12-02 11:34:43 +0100

    Refactor structure

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,20 +1,25 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import Comments from './Comments';
 import OpinionForm from './OpinionForm';
 import Product from './Product';
 export default function Home() {
   return (
-    <div className="w-[60vw] m-auto flex flex-col items-center">
-      <h1 className="m-8 tracking-wider text-6xl font-bold">Hackathon App</h1>
-      <div className="m-4">
-        <Product />
-      </div>
-      <div>
-        <OpinionForm />
-      </div>
-      <div className="m-4">
-        <Comments />
-      </div>
+    <div>
+      <Link className="absolute right-0 my-2 mx-4 py-2 px-4 rounded-full bg-rose-600" href="/admin">Admin</Link>
+      <main className="w-[60vw] m-auto flex flex-col items-center gap-8">
+        <h1 className="p-4 tracking-wider text-6xl font-bold">Hackathon App</h1>
+        <div>
+          <Product />
+        </div>
+        <div>
+          <OpinionForm />
+        </div>
+        <div>
+          <Comments />
+        </div>
+      </main>
     </div>
+    
   )
 }
